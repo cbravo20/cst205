cd95e699490cc3337b8df5be5c3af5e91c95cb48
cbravo20 <cbravo-perez@csumb.edu>
2026-03-24 11:48:35 -0700

    adding lab

---
+++ lab15.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+def hello():
+    return 'Hello world from Flask!'
+
+@app.route('/welcome')
+def wc():
+    s1 = 'Welcome to my page! '
+    s2 = 'Have a nice day'
+    return s1 + s2
+
+@app.route('/mytemplate')
+def t_test():
+    return render_template('template.html')
+
+@app.route('/cris')
+def cris():
+    return render_template('cris.html')
